6ef3dab27f65fef9ce382276eb4d882c3323bd41
Liu <jliu5@HFHS.ORG>
2017-10-17 08:08:59 -0400

    add todo list

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -1,10 +1,35 @@
 var React = require('react');
+var TodoList =require('TodoList');
 
 var TodoApp = React.createClass({
+	getInitialState: function(){
+		return{
+			todos: [
+			{
+				id:1,
+				text: 'IT support'
+			},
+			{
+				id: 2,
+				text: 'write the code'
+			},
+			{
+				id: 3,
+				text: 'learn new technique'
+			},
+			{
+				id: 4,
+				text: 'listen to music'
+			}
+			]
+		}
+	},
 	render: function(){
+		var {todos} = this.state;
+
 		return (
 			<div>
-				<p>this is TodoApp.jsx</p>
+				<TodoList todos={todos}/>
 			</div>
 		);
 	}
